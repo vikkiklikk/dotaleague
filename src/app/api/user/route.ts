105739c1bb4900c4d97135f8bee7d3925fbad3c6
vikkiklikk <viktor.birgisson@gmail.com>
2024-03-26 23:04:37 +0000

    added prisma adapter as well as credentials as a provider to the sign up form

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,12 +1,24 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { hash } from 'bcrypt';
-import { Hash } from "crypto";
+import * as z from 'zod';
+
+// Define a schema for input validation
+
+const userSchema = z
+  .object({
+    username: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(8, 'Password must have than 8 characters'),
+  })
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, username, password } = body;
+    const { email, username, password } = userSchema.parse(body);
 
     // check if the email already exists
     const existingUserByEmail = await db.user.findUnique({
